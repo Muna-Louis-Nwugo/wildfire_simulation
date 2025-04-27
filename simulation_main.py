d80f0984d8fcba1ccd20c957d1d33e2c31a1f3a9
MunaNwugo <munachimsonwugo@gmail.com>
2025-04-27 18:19:54 -0400

    Update simulation_main.py

--- simulation_main.py
+++ simulation_main.py
@@ -1,1 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+# Define data types for the simulation
+
+cell_dtypes = np.dtype([
+    ('type', np.int8), # 0: empty, 1: forest, 2: building, 3: road
+    ('on_fire', np.bool_) # Whether the cell is on fire
+    ])
+
+
+# construct the first simulation grid
+grid = np.zeros((20, 20), dtype=cell_dtypes)
+
+print(grid)
